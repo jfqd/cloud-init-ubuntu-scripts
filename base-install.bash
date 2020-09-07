b86ebc52af0c9634c1403a7c51daf5cff1126d37
jfqd <jfqd@blun.org>
2020-09-07 21:59:09 +0200

    log what we are doing

--- base-install.bash
+++ base-install.bash
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 path=$(realpath $0)
+
+echo "switch to folder: $path"
 cd "$(dirname "$path")"
 
+export DEBIAN_FRONTEND=noninteractive
+
+echo "set hostname"
 ./set-hostname.bash
+
+echo "remove unattended-upgrades"
+./apt-config.bash
+
+echo "get latest upgrades"
 ./apt-upgrade.bash
+
+echo "install exim"
 ./install-exim4.bash
+
+echo "install zabbix"
 ./install-zabbix-agent.bash
