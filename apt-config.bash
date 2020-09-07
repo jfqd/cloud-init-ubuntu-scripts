273b70934a15c3862af618cd8ac7105dc50b403e
jfqd <jfqd@blun.org>
2020-09-07 21:57:51 +0200

    remove unattended-upgrades

---
+++ apt-config.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'APT::Periodic::Enable 0;' > /etc/apt/apt.conf.d/10cloudinit-disable
+apt-get -y purge update-notifier-common ubuntu-release-upgrader-core landscape-common unattended-upgrades
+rm -rf /var/log/unattended-upgrades
+apt -y autoremove
