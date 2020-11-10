f3124950a43717fe3a59569085be1755bcff8da2
jfqd <jfqd@blun.org>
2020-11-10 17:05:52 +0100

    add status page

--- install-nginx.bash
+++ install-nginx.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 apt -y -qq install nginx
+
+cat  >> /etc/nginx/conf.d/status.conf << EOF
+server {
+  listen 127.0.0.1;
+  server_name localhost;
+  location /nginx_status {
+    stub_status on;
+    access_log   off;
+    allow 127.0.0.1;
+    deny all;
+  }
+}
+EOF
