38f0fdc46148d90630dc4891a5263cd4ec329b90
jfqd <jfqd@blun.org>
2021-02-24 20:53:26 +0100

    use regular prosody

--- install-jitsi-with-pwd.bash
+++ install-jitsi-with-pwd.bash
@@ -6,19 +6,28 @@ sed -i "s/#DefaultLimitNOFILE=/DefaultLimitNOFILE=65000/" /etc/systemd/system.co
 sed -i "s/#DefaultLimitNPROC=/DefaultLimitNPROC=65000/" /etc/systemd/system.conf
 sed -i "s/#DefaultTasksMax=/DefaultTasksMax=65000/" /etc/systemd/system.conf
 
+# echo deb http://packages.prosody.im/debian $(lsb_release -sc) main | tee -a /etc/apt/sources.list
+# wget https://prosody.im/files/prosody-debian-packages.key -O- | apt-key add -
+# apt-get update
+# apt-get -y -qq install prosody
+
 curl -Ls https://download.jitsi.org/jitsi-key.gpg.key | apt-key add -
 echo 'deb https://download.jitsi.org stable/' > /etc/apt/sources.list.d/jitsi-stable.list
-apt-get -y update
+apt-get update
 
 # https://github.com/jitsi/jitsi-meet/issues/5759
 echo "jitsi-videobridge jitsi-videobridge/jvb-hostname string ${HOSTNAME}" | debconf-set-selections
 echo "jitsi-meet-web-config jitsi-meet/cert-choice select 'Generate a new self-signed certificate (You will later get a chance to obtain a Let's encrypt certificate)'" | debconf-set-selections
 apt-get --option=Dpkg::Options::=--force-confold --option=Dpkg::options::=--force-unsafe-io --assume-yes --quiet install jitsi-meet
 
+echo "Include \"conf.d/*.cfg.lua\"" >> /etc/prosody/prosody.cfg.lua
+
 sed -i 's|read EMAIL|EMAIL=$(/usr/sbin/mdata-get mail_adminaddr)|' /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
 /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
 
 cp /etc/prosody/conf.avail/jm-${HOSTNAME}.cfg.lua /etc/prosody/conf.avail/jm-${HOSTNAME}.cfg.lua.orig
+
+sed -i -e "s#authentication = \"anonymous\"#authentication = \"internal_plain\"#" /etc/prosody/conf.avail/${HOSTNAME}.cfg.lua
 
 cat >> /etc/prosody/conf.avail/${HOSTNAME}.cfg.lua << EOF
 
@@ -61,12 +70,11 @@ videobridge {
     }
     websockets {
         enabled = true
-        domain = "jitsi.example.com:443"
+        domain = "${HOSTNAME}:443"
         tls = true
     }
 }
 EOF
-sed -i -e "s/jitsi.example.com/${HOSTNAME}/" /etc/jitsi/videobridge/jvb.conf
 
 # zabbix monitoring
 cat >> /etc/zabbix/zabbix_agentd.d/local.conf << EOF
@@ -75,7 +83,7 @@ EOF
 systemctl restart zabbix-agent
 
 # restart services
-service jicofo restart
-service jitsi-videobridge2 restart
-service prosody restart
-service nginx restart
+systemctl restart prosody
+systemctl restart jicofo
+systemctl restart jitsi-videobridge2
+systemctl restart nginx
