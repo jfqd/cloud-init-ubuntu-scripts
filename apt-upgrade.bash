f1f449a1cc24a7f47b94884d89367644a26cfadf
jfqd <jfqd@blun.org>
2021-03-01 10:53:19 +0100

    add uptodate script

--- apt-upgrade.bash
+++ apt-upgrade.bash
@@ -4,3 +4,13 @@ apt-get -y update
 yes no | apt-get -y -qq upgrade
 
 # apt-get -y -qq dist-upgrade
+
+cat >> /usr/local/bin/uptodate << EOF
+#!/bin/bash
+
+/usr/bin/apt update
+/usr/bin/apt -y upgrade
+/usr/bin/apt -y autoremove
+
+EOF
+chmod +x /usr/local/bin/uptodate
