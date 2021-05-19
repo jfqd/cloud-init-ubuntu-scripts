83feb5acea28efada012e0fa4d9c86986fff4b2b
jfqd <jfqd@blun.org>
2021-05-19 10:58:46 +0200

    outsource command

--- base-install.bash
+++ base-install.bash
@@ -10,17 +10,14 @@ export DEBIAN_FRONTEND=noninteractive
 echo "*** Set hostname"
 ./set-hostname.bash
 
+echo "*** Update ubuntu user"
+./configure-user-secret.bash
+
 echo "*** Get latest upgrades"
 ./apt-upgrade.bash
 
 echo "*** Install exim"
 ./install-exim4.bash
-
-echo "*** Update ubuntu user"
-if /usr/sbin/mdata-get ubuntu_user_secret 1>/dev/null 2>&1; then
-  SECRET=$(/usr/sbin/mdata-get ubuntu_user_secret)
-  /usr/sbin/usermod --password "\$6\$.${SECRET}" ubuntu
-fi
 
 echo "*** Install zabbix"
 ./install-zabbix-agent.bash
---
+++ configure-user-secret.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if /usr/sbin/mdata-get ubuntu_user_secret 1>/dev/null 2>&1; then
+  SECRET=$(/usr/sbin/mdata-get ubuntu_user_secret)
+  /usr/sbin/usermod --password "\$6\$.${SECRET}" ubuntu
+fi
