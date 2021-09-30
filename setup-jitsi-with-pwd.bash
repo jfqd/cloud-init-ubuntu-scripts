e01b4bd1daf8d7f600d50b9771eead9fddab30fb
jfqd <jfqd@blun.org>
2021-09-30 13:23:55 +0200

    add log output

--- setup-jitsi-with-pwd.bash
+++ setup-jitsi-with-pwd.bash
@@ -5,10 +5,18 @@
 cd $(dirname $(readlink -f $0))
 
 ./base-install.bash
+
+echo "*** Deactivate ipv6"
 ./deactivate-ipv6.bash
+
+echo "*** Setup uwf"
 ./install-uwf-for-jitsi.bash
 ./configure-uwf-for-zabbix.bash
+
+echo "*** Install jitsi"
 ./install-jitsi-with-pwd.bash
+
+echo "*** Install nginx"
 ./install-nginx.bash
 
 )
