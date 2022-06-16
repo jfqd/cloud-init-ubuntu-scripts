8caf36dbdd2009afa6dca470e2b9887c17ef40c6
jfqd <jfqd@blun.org>
2022-06-16 16:42:23 +0200

    fix hostname

--- set-hostname.bash
+++ set-hostname.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+IP=$(ip address show dev net0 | grep 'inet ' | awk '{print $2}' | sed 's|/.*||')
 HOSTNAME=$(/usr/sbin/mdata-get sdc:hostname)
-sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $(hostname) ${HOSTNAME}/" /etc/hosts
+echo "${IP}  %{HOSTNAME}" >> /etc/hosts
 sed -i "s/preserve_hostname: false/preserve_hostname: true/" /etc/cloud/cloud.cfg
 hostnamectl set-hostname $HOSTNAME
