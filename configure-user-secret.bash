fd359aa37e0d561b9ca8b2a9343ed819c95fd99c
jfqd <jfqd@blun.org>
2023-11-27 18:01:10 +0100

    add sudo group too

--- configure-user-secret.bash
+++ configure-user-secret.bash
@@ -3,7 +3,7 @@
 if [[ "$(id ubuntu 2>/dev/null; echo $?)" = "1" ]]; then
   addgroup ubuntu
   adduser --disabled-password --system --quiet --home /home/ubuntu --shell /usr/bin/bash ubuntu
-  adduser ubuntu ubuntu
+  usermod -G ubuntu,sudo ubuntu
   mkdir -p /home/ubuntu
 fi
 
