8c7774d8c1cd48a62d00a73999146d2c165ea71f
jfqd <jfqd@blun.org>
2023-11-27 16:26:01 +0100

    create ubuntu user if missing

--- configure-user-secret.bash
+++ configure-user-secret.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ "$(id ubuntu 2>/dev/null; echo $?)" = "1" ]]; then
+  addgroup ubuntu
+  adduser --disabled-password --system --quiet --home /home/ubuntu --shell /usr/bin/bash ubuntu
+  adduser ubuntu ubuntu
+  mkdir -p /home/ubuntu
+fi
+
 if /usr/sbin/mdata-get ubuntu_user_secret 1>/dev/null 2>&1; then
   SECRET=$(/usr/sbin/mdata-get ubuntu_user_secret)
   /usr/sbin/usermod --password "\$6\$${SECRET}" ubuntu
