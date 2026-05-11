a61c5fff329c8d7137cee637b4f650ec4e24652c
jfqd <jfqd@blun.org>
2026-05-11 11:35:32 +0200

    set sshd config directly

--- base/harden_sshd.bash
+++ base/harden_sshd.bash
@@ -1,15 +1,34 @@
 #!/usr/bin/bash
 
-sed -i \
-    -e "s/PermitRootLogin yes/PermitRootLogin no/" \
-    -e "s/PasswordAuthentication yes/PasswordAuthentication no/" \
-    -e "s/UsePAM yes/UsePAM no/" \
-    -e "s/X11Forwarding yes/X11Forwarding no/" \
-    /etc/ssh/sshd_config
-
-cat >> /etc/ssh/sshd_config << EOF
-
-ChallengeResponseAuthentication no
+mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+cat > /etc/ssh/sshd_config << 'EOF'
+# qutic-base sshd_config
+Port 22
+AddressFamily any
+Protocol 2
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+SyslogFacility AUTH
+LogLevel INFO
+LoginGraceTime 60
+PermitRootLogin yes
+StrictModes yes
+MaxAuthTries 3
+AuthorizedKeysFile .ssh/authorized_keys
+PasswordAuthentication no
+PermitEmptyPasswords no
+KeepAlive yes
+AllowAgentForwarding no
+AllowTcpForwarding no
+GatewayPorts no
+X11Forwarding no
+X11UseLocalhost no
+PrintMotd no
+PermitUserEnvironment no
+UseDNS no
+VersionAddendum All your SSH belong to us
+Subsystem       sftp    /usr/lib/openssh/sftp-server
 EOF
 
 sed -i \
